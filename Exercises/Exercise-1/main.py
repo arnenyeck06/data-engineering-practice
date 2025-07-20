a8410edb4131726928b821afcc9a4a9985e3f5e5
Arne Nyeck Nyeck <arnenyeck06@email.com>
2025-07-20 16:53:41 -0500

    Downloading files with python

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,6 +1,9 @@
 import requests
+import os
+from zipfile import ZipFile
+#import validators
 
-download_uris = [
+url = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
@@ -10,11 +13,44 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+def is_url_valid(url):
+    try:
+        response = requests.head(url, timeout = 5)
+        return response.status_code == 200
+    except requests.RequestException as e:
+        print(f" Url is not valid: {url}: {e}")
+        return False
+
+def download_files(urls, download_dir):
+    for link in urls:
+        if is_url_valid(link):
+            try:
+                response = requests.get(link) 
+                response.raise_for_status()
+                filename = link.split('/')[-1]
+
+                file_path = os.path.join(download_dir, filename)
+                with open(file_path, "wb") as f:
+                    f.write(response.content)
+                print(f"downloaded files: {filename}")
+                
+                with ZipFile(file_path, 'r') as zip_ref:
+                    zip_ref.extractall(download_dir)
+                os.remove(file_path)
+                print(f" Extracted and removed:{filename}")
+
+            except requests.exceptions.RequestException as e:
+                print(f"Error {link}: {e}")
+        else:
+            print(f" skipped URL: {link}")
 
 def main():
-    # your code here
-    pass
-
+    download_dir = os.path.join(os.path.dirname(__file__), "downloads")
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+    download_files(url, download_dir)
 
 if __name__ == "__main__":
     main()
+
+
